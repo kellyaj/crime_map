5e68d3bf00d5b8d417e0eafb969773de6bbc35a4
Andrew Kelly <kellyaj@gmail.com>
2014-02-20 00:50:16 -0600

    wip grunt watch

--- gruntfile.js
+++ gruntfile.js
@@ -9,11 +9,18 @@ module.exports = function(grunt) {
           ".tmp/js/templates.js": ["js/**/*.ejs"]
         }
       }
+    },
+    watch: {
+     jst: {
+        files: ["js/templates/*.ejs"],
+        tasks: ["jst"]
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-jst');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['jst']);
 }
