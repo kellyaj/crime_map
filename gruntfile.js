8434a632af48c222682b3f805cf497880ac6fdee
Andrew Kelly <akelly@8thlight.com>
2014-02-27 15:49:29 -0600

    introduces coffeescript compilation

--- gruntfile.js
+++ gruntfile.js
@@ -15,10 +15,29 @@ module.exports = function(grunt) {
         files: ["js/templates/*.ejs"],
         tasks: ["jst"]
       }
+    },
+    coffee: {
+      compileScripts: {
+        expand: true,
+        flatten: true,
+        cwd: 'js',
+        src: '**/*.coffee',
+        dest: '.tmp/scripts',
+        ext: '.js'
+      },
+      compileSpecs: {
+        expand: true,
+        flatten: true,
+        cwd: 'spec',
+        src: '**/*.coffee',
+        dest: '.tmp/spec',
+        ext: '.js'
+      }
     }
 
   });
 
+  grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-jst');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
